294757b7976b27e7f2b5e97e8e3ed18087f3e028
dywork <dubrovskywork@gmail.com>
2020-01-21 19:34:30 +0500

    style(karma): форматирование по airbnb

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,28 +1,28 @@
-const webpackConfig = require("../webpack.config");
+import webpackConfig from '../webpack.config';
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine"],
-    files: ["../src/**/*.test.ts"],
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: ['../src/**/*.test.ts'],
     exclude: [],
     preprocessors: {
-      "../src/**/*.test.ts": ["webpack"],
-      "../src/**/*.test.js": ["webpack"]
+      '../src/**/*.test.ts': ['webpack'],
+      '../src/**/*.test.js': ['webpack'],
     },
     webpack: {
       module: webpackConfig.module,
       resolve: webpackConfig.resolve,
       mode: webpackConfig.mode,
-      devtool: "inline-source-map"
+      devtool: 'inline-source-map',
     },
-    reporters: ["spec"],
+    reporters: ['spec'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ['Chrome'],
     singleRun: false,
-    concurrency: Infinity
+    concurrency: Infinity,
   });
 };
