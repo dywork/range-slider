2c8c26d284e49c27e075ad5d5f089cc320aaa62d
dywork <dubrovskywork@gmail.com>
2020-01-22 15:13:19 +0500

    fix(model.test): передаю min и max валуе в конструктор

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -3,13 +3,15 @@ import { Model, IModel } from './Model';
 describe('Model', () => {
   it('Возвращает заданый min value', () => {
     const minValue = 5;
-    const model: IModel = new Model(minValue);
+    const maxValue = 115;
+    const model: IModel = new Model(minValue, maxValue);
     expect(model.getMinValue()).toBe(minValue);
   });
 
   it('Возвращает заданый max value', () => {
+    const minValue = 5;
     const maxValue = 115;
-    const model: IModel = new Model(maxValue);
+    const model: IModel = new Model(minValue, maxValue);
     expect(model.getMaxValue()).toBe(maxValue);
   });
 });
