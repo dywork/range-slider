3af7041e3ed2fc70494c9b5ff4c70ca681073156
dywork <dubrovskywork@gmail.com>
2020-01-22 15:09:53 +0500

    refactor(model.test):  удалил устаревший тест

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -1,12 +1,6 @@
 import { Model, IModel } from './Model';
 
 describe('Model', () => {
-  it('Возвращает заданый текст', () => {
-    const text = 'Привет, мир!';
-    const model: IModel = new Model(text);
-    expect(model.getText()).toBe(text);
-  });
-
   it('Возвращает заданый min value', () => {
     const minValue = 5;
     const model: IModel = new Model(minValue);
