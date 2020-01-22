eff74cec92329db73f64173d5de4ed433bab1c31
dywork <dubrovskywork@gmail.com>
2020-01-22 15:11:10 +0500

    test(model): проверяет возвращение заданного max value

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -6,4 +6,10 @@ describe('Model', () => {
     const model: IModel = new Model(minValue);
     expect(model.getMinValue()).toBe(minValue);
   });
+
+  it('Возвращает заданый max value', () => {
+    const maxValue = 115;
+    const model: IModel = new Model(maxValue);
+    expect(model.getMaxValue()).toBe(maxValue);
+  });
 });
