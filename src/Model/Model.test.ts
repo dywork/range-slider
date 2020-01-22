7c27afa9c6db0de98d0b56bc27b1754d05eaec0b
dywork <dubrovskywork@gmail.com>
2020-01-22 15:20:29 +0500

    refactor(model.test): вынес объявление инстанса модели в beforeEach

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -1,17 +1,20 @@
 import { Model, IModel } from './Model';
 
+let model: IModel;
+beforeEach(() => {
+  const minValue = 5;
+  const maxValue = 115;
+  model = new Model(minValue, maxValue);
+});
+
 describe('Model', () => {
   it('Возвращает заданый min value', () => {
     const minValue = 5;
-    const maxValue = 115;
-    const model: IModel = new Model(minValue, maxValue);
     expect(model.getMinValue()).toBe(minValue);
   });
 
   it('Возвращает заданый max value', () => {
-    const minValue = 5;
     const maxValue = 115;
-    const model: IModel = new Model(minValue, maxValue);
     expect(model.getMaxValue()).toBe(maxValue);
   });
 });
