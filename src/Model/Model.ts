f8ba18f5c5c82d09928254d2799f91e4790ec4fb
dywork <dubrovskywork@gmail.com>
2020-01-22 15:13:39 +0500

    feat(model): добавил метод возвращающий заданный maxValue

--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -1,15 +1,21 @@
 interface IModel {
   getMinValue(): number;
+  getMaxValue(): number;
 }
 
 class Model implements IModel {
   private minValue: number;
 
-  constructor(minValue: number) {
+  private maxValue: number;
+
+  constructor(minValue: number, maxValue: number) {
     this.minValue = minValue;
+    this.maxValue = maxValue;
   }
 
   getMinValue = () => this.minValue;
+
+  getMaxValue = () => this.maxValue;
 }
 
 export { Model, IModel };
