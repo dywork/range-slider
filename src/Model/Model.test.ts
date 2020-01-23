203ce361fd5359c4b4efd51deb21546bcd86a068
dywork <dubrovskywork@gmail.com>
2020-01-23 16:47:14 +0500

    test(model.test): тестирует получени заданных значений range

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -1,36 +1,9 @@
 import { Model, IModel } from './Model';
 
-let model: IModel;
-beforeEach(() => {
-  model = new Model();
-});
-
 describe('Model', () => {
-  it('Возвращает заданный value.min', () => {
-    const min = 20;
-    const myModel = new Model({ min });
-    expect(myModel.getRange().min).toBe(min);
-  });
-
-  it('Возвращает заданный value.max', () => {
-    const max = 80;
-    const myModel = new Model({ max });
-    expect(myModel.getRange().max).toBe(max);
-  });
-
-  it('Выдает ошибку если заданный minValue больше текущего maxValue', () => {
-    const maxValue = 5;
-    const minValue = 115;
-    model.setMaxValue(maxValue);
-    const func = model.setMinValue.bind(this, minValue);
-    expect(func).toThrowError('minValue не может быть > maxValue');
-  });
-
-  it('Выдает ошибку если заданный maxValue меньше текущего minValue', () => {
-    const maxValue = 5;
-    const minValue = 99;
-    model.setMinValue(minValue);
-    const func = model.setMaxValue.bind(this, maxValue);
-    expect(func).toThrowError('maxValue не может быть < minValue');
+  it('возвращает заданные min и max range value', () => {
+    const model: IModel = new Model({ min: 0, max: 100 });
+    expect(model.getRange().min).toBe(0);
+    expect(model.getRange().max).toBe(100);
   });
 });
