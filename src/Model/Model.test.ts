3e19ee1d8e31e7b75379a939485538e7e7250777
dywork <dubrovskywork@gmail.com>
2020-01-23 10:03:27 +0500

    refactor(model.test): удалены устаревшие тесты

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -6,18 +6,6 @@ beforeEach(() => {
 });
 
 describe('Model', () => {
-  it('Присваивает заданный minValue', () => {
-    const minValue = 10;
-    model.setMinValue(minValue);
-    expect(model.getMinValue()).toBe(minValue);
-  });
-
-  it('Присваивает заданный maxValue', () => {
-    const maxValue = 50;
-    model.setMaxValue(maxValue);
-    expect(model.getMaxValue()).toBe(maxValue);
-  });
-
   it('Возвращает заданный value.min', () => {
     const min = 20;
     const myModel = new Model({ min });
