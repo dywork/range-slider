7233ec7ede271c6d0911a74df64ee7dfe6afaf41
dywork <dubrovskywork@gmail.com>
2020-01-27 16:37:01 +0500

    refactor(model.test): вынес инициализацию model в beforeEach

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -1,28 +1,27 @@
-import { Model } from './Model';
+import { Model, IModel } from './Model';
+
+const min = 0;
+const max = 100;
+const start = 0;
+let model: IModel;
+beforeEach(() => {
+  model = new Model(start, { min, max });
+});
 
 describe('Model', () => {
   it('возвращает заданное начальное значение', () => {
-    const min = 0;
-    const max = 100;
-    const start = 0;
-    const model = new Model(start, { min, max });
     expect(model.getStart()).toBe(start);
   });
 
   it('возвращает заданные range.min и range.max', () => {
-    const min = 0;
-    const max = 100;
-    const model = new Model({ min, max });
     expect(model.getRange().min).toBe(min);
     expect(model.getRange().max).toBe(max);
   });
 
   it('сообщает об ошибке если range.min > range.max', () => {
-    const min = 100;
-    const max = 0;
     expect(() => {
-      const model = new Model({ min, max });
-      return model;
+      const myModel = new Model(start, { min: max, max: min });
+      return myModel;
     }).toThrowError('range.min не может быть > range.max');
   });
 });
