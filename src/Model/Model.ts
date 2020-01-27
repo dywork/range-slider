20145b5cbbb5da38eb2f95b7d312413e3c5e3b38
dywork <dubrovskywork@gmail.com>
2020-01-27 16:31:14 +0500

    feat(model): возвращает значение начального положения

--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -8,15 +8,18 @@ interface IRange {
 }
 
 class Model implements IModel {
+  private start: number;
+
   private range: IRange;
 
-  constructor(range: IRange) {
+  constructor(start: number, range: IRange) {
     const { min, max } = range;
 
     if (min > max) {
       this.throwError('range.min не может быть > range.max');
     }
 
+    this.start = start;
     this.range = {
       min,
       max,
@@ -24,6 +27,8 @@ class Model implements IModel {
   }
 
   getRange = () => this.range;
+
+  getStart = () => this.start;
 
   private throwError = (errorMsg: string) => {
     throw new Error(errorMsg);
