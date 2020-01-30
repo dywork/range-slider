4ea8fdd8fb710dd125b04c289ff7011070e9acfe
dywork <dubrovskywork@gmail.com>
2020-01-30 21:16:22 +0500

    build(presenter): добавил presenter

---
+++ src/Presenter/Presenter.ts
@@ -0,0 +1,17 @@
+import { Model } from '../Model/Model';
+import View from '../View/View';
+
+class Presenter {
+  init = () => {
+    const start = 0;
+    const range = {
+      min: 0,
+      max: 100,
+    };
+    const model = new Model(start, range);
+    const view = new View(model.getRange());
+    view.render();
+  };
+}
+
+export default Presenter;
