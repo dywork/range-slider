60ec16682d9a3e019bac373e95b3ca0998af313f
dywork <dubrovskywork@gmail.com>
2020-01-30 21:20:17 +0500

    feat(view): добавил отображение значения start

--- src/View/View.ts
+++ src/View/View.ts
@@ -1,13 +1,19 @@
 import { IRange } from '../Model/Model';
 
 class View {
+  private start: number;
+
   private range: IRange;
 
-  constructor(range: IRange) {
+  constructor(start: number, range: IRange) {
+    this.start = start;
     this.range = range;
   }
 
   render = () => {
+    const startHTML = document.createElement('p');
+    startHTML.textContent = `начальное значение: ${this.start}`;
+    document.body.appendChild(startHTML);
     const p = document.createElement('p');
     p.textContent = `${this.range.min} --- ${this.range.max}`;
     document.body.appendChild(p);
