e408a0c8c413d2c61d41b939a5c2d2a6687ddaf8
dywork <dubrovskywork@gmail.com>
2020-02-08 20:11:04 +0500

    feat(Presenter): передаю во view currentValue

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -12,7 +12,7 @@ class Presenter {
 
   init = () => {
     const model = new Model(this.options);
-    const view = new View(model.getStart(), model.getRange());
+    const view = new View(model.getStart(), model.getRange(), model.getCurrentValue());
     view.render();
   };
 }
