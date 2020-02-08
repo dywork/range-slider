e4abcd2ef6bbb969ea02c8189d8a378e77dad031
dywork <dubrovskywork@gmail.com>
2020-02-08 11:38:14 +0500

    feat(index.ts): обернул api слайдера в jqurey

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
-import Presenter from './Presenter/Presenter';
+/* eslint-disable func-names */
+import { Presenter, IOptions } from './Presenter/Presenter';
 
-const presenter = new Presenter();
-presenter.init();
+declare global {
+  interface JQuery {
+    rangeSlider(options: IOptions): void;
+  }
+}
+
+(function (jquery) {
+  const $ = jquery;
+  $.fn.rangeSlider = function (options) {
+    let sliderOptions = options;
+    // Устанавливаем значения по умолчанию
+    sliderOptions = $.extend(
+      {
+        start: 0,
+        range: {
+          min: 0,
+          max: 100,
+        },
+      },
+      sliderOptions,
+    );
+    // Выбранный элемент будет в this, т.е. это уже объект JQuery, а не элемент DOM
+    // Код плагина (может быть на JS)
+    const presenter = new Presenter(sliderOptions);
+    presenter.init();
+  };
+}(jQuery));
