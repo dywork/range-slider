bf2d8775aa07222ed66b36820d2096650d35557c
dywork <dubrovskywork@gmail.com>
2020-02-12 12:34:40 +0500

    test(observer): добавлен тест на удаление конкретного observer

--- src/Observer/Observer.test.ts
+++ src/Observer/Observer.test.ts
@@ -11,4 +11,24 @@ describe('Observer', () => {
     observer.addObserver(observerEvent);
     expect(observer.getCount()).toBe(1);
   });
+
+  it('Удаляет конкретный observer', () => {
+    const observer = new Observer();
+    const observerEventOne = () => {
+      const message = 'Сообщение из observer.test №1';
+      // eslint-disable-next-line no-console
+      console.log(message);
+    };
+    const observerEventSecond = () => {
+      const message = 'Сообщение из observer.test №2';
+      // eslint-disable-next-line no-console
+      console.log(message);
+    };
+    observer.addObserver(observerEventOne);
+    observer.addObserver(observerEventSecond);
+    expect(observer.getCount()).toBe(2);
+    observer.removeObserver(observerEventOne);
+    expect(observer.getCount()).toBe(1);
+    expect(observer.getList()[0]).toBe(observerEventSecond);
+  });
 });
