08960eb076f318d2682c357d6eee719b1471e328
dywork <dubrovskywork@gmail.com>
2020-03-28 13:18:05 +0500

    feat(index): инициализирую mvp

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import { Model } from './Model/Model';
 import { View } from './View/View';
+import { Presenter } from './Presenter/Presenter';
+
+const myModel = new Model({ currentValue: 0, minValue: 0, maxValue: 100 });
+const myView = new View(myModel.getOptions());
+const myPresenter = new Presenter(myModel, myView);
+myPresenter.init();
