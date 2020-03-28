d631a67cabbcb41ce900843474c6657f0e676586
dywork <dubrovskywork@gmail.com>
2020-03-28 13:35:30 +0500

    feat(observer): описал observer

--- src/Observer/Observer.ts
+++ src/Observer/Observer.ts
@@ -1,4 +1,5 @@
 interface IObserver {
+  getObservers(): Function[];
   add(observer: Function): void;
   remove(observer: Function): void;
   notify(data?: Object): void;
@@ -10,6 +11,31 @@ class Observer implements IObserver {
   constructor() {
     this.observers = [];
   }
+
+  getObservers = () => this.observers;
+
+  add = (observer: Function) => {
+    this.getObservers().push(observer);
+  };
+
+  remove = (observer: Function) => {
+    const index = this.getObservers().indexOf(observer);
+    const isFind = index > -1;
+
+    if (isFind) {
+      this.getObservers().splice(index, 1);
+    }
+  };
+
+  notify = (data?: Object) => {
+    const isHaveObservers = this.getObservers().length > 0;
+
+    if (isHaveObservers) {
+      this.getObservers().forEach((observer: Function) => {
+        observer(data);
+      });
+    }
+  };
 }
 
 export default Observer;
