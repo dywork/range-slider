ab2179a0eb8e8b58a150139e01a695499afbc30e
dywork <dubrovskywork@gmail.com>
2020-03-28 12:36:57 +0500

    feat(view): добавил интерфейс IView

--- src/View/View.ts
+++ src/View/View.ts
@@ -1,10 +1,17 @@
+interface IView {
+  renderCurrentValue(): void;
+  renderMinValue(): void;
+  renderMaxValue(): void;
+  render(): void;
+}
+
 interface IViewOptions {
   currentValue: number;
   minValue: number;
   maxValue: number;
 }
 
-class View {
+class View implements IView {
   private viewOptions: IViewOptions;
 
   constructor(viewOptions: IViewOptions) {
