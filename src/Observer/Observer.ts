b8f3ccd83bd127f64c9715afa31b68baa4bbeb2c
dywork <dubrovskywork@gmail.com>
2020-03-28 13:30:12 +0500

    feat(observer): описал interface

--- src/Observer/Observer.ts
+++ src/Observer/Observer.ts
@@ -1,4 +1,10 @@
-class Observer {
+interface IObserver {
+  add(observer: Function): void;
+  remove(observer: Function): void;
+  notify(data?: Object): void;
+}
+
+class Observer implements IObserver {
   private observers: Function[];
 
   constructor() {
