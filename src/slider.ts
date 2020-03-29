1f27886d12a2933e70b6b4a72766f2a5cbe17477
dywork <dubrovskywork@gmail.com>
2020-03-29 13:57:35 +0500

    feat(slider): создает только инстанс presentrа

--- src/slider.ts
+++ src/slider.ts
@@ -1,20 +1,12 @@
-import { Model, IModel } from './Model/Model';
-import { View, IView } from './View/View';
 import { Presenter, IPresenter } from './Presenter/Presenter';
 import Observer from './Observer/Observer';
 
 class Slider extends Observer {
-  private model: IModel;
-
-  private view: IView;
-
   private presenter: IPresenter;
 
   constructor() {
     super();
-    this.model = new Model({ currentValue: 0, minValue: 0, maxValue: 100 });
-    this.view = new View(this.model.getOptions());
-    this.presenter = new Presenter(this.model, this.view);
+    this.presenter = new Presenter({ currentValue: 0, minValue: 0, maxValue: 100 });
   }
 
   init = () => {
