2228bd0065413bf10c14d949a7d8d6c2c9fa2d89
dywork <dubrovskywork@gmail.com>
2020-03-29 15:42:30 +0500

    feat(presenter): расширяю класс presenter observerом

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -1,17 +1,19 @@
 import { Model, IModel } from '../Model/Model';
 import { View, IView } from '../View/View';
 import ISliderOptions from '../interfaces/ISliderOptions';
+import Observer from '../Observer/Observer';
 
 interface IPresenter {
   init(): void;
 }
 
-class Presenter implements IPresenter {
+class Presenter extends Observer implements IPresenter {
   private model: IModel;
 
   private view: IView;
 
   constructor(sliderOptions: ISliderOptions) {
+    super();
     this.model = new Model(sliderOptions);
     this.view = new View(this.model.getOptions());
   }
