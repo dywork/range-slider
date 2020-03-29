52452291565dd58def17dfc13447df38085313c7
dywork <dubrovskywork@gmail.com>
2020-03-29 13:42:28 +0500

    feat(slider): обернул инициализацию mvp в класс

--- src/slider.ts
+++ src/slider.ts
@@ -1,8 +1,16 @@
 import { Model } from './Model/Model';
 import { View } from './View/View';
 import { Presenter } from './Presenter/Presenter';
+import Observer from './Observer/Observer';
 
-const myModel = new Model({ currentValue: 0, minValue: 0, maxValue: 100 });
-const myView = new View(myModel.getOptions());
-const myPresenter = new Presenter(myModel, myView);
-myPresenter.init();
+class Slider extends Observer {
+  init = () => {
+    const myModel = new Model({ currentValue: 0, minValue: 0, maxValue: 100 });
+    const myView = new View(myModel.getOptions());
+    const myPresenter = new Presenter(myModel, myView);
+    myPresenter.init();
+  };
+}
+
+const slider = new Slider();
+slider.init();
