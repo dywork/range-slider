51de39c7678863b4efc2ddaa25b8513bc71d626f
dywork <dubrovskywork@gmail.com>
2020-03-30 12:27:31 +0500

    refactor(model.test): вынес создание инстанса модели и настроек в beforeEach

--- src/Model/model.test.ts
+++ src/Model/model.test.ts
@@ -1,10 +1,16 @@
 import Model from './Model';
 import ISliderOptions from '../interfaces/ISliderOptions';
 
+let sliderOptions: ISliderOptions;
+let model: Model;
+
+beforeEach(() => {
+  sliderOptions = { currentValue: 0, range: { min: 0, max: 100 } };
+  model = new Model(sliderOptions);
+});
+
 describe('Model', () => {
   it('возвращает текущие настройки слайдера', () => {
-    const sliderOptions: ISliderOptions = { currentValue: 0, range: { min: 0, max: 100 } };
-    const model = new Model(sliderOptions);
     expect(model.getOptions()).toBe(sliderOptions);
   });
 });
