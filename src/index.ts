ee41d9eebf435244e650aa1e80738b2b001c4e9a
dywork <dubrovskywork@gmail.com>
2020-03-30 10:31:37 +0500

    feat(index): добавил точку входа

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import Slider from './Slider';
+import ISliderOptions from './interfaces/ISliderOptions';
+
+declare global {
+  interface JQuery {
+    rangeSlider(options: ISliderOptions): void;
+  }
+}
+
+(function (jquery) {
+  const $ = jquery;
+  $.fn.rangeSlider = function (options) {
+    let sliderOptions = options;
+    // Устанавливаем значения по умолчанию
+    sliderOptions = $.extend(
+      {
+        start: 0,
+        currentValue: 0,
+        range: {
+          min: 0,
+          max: 100,
+        },
+      },
+      sliderOptions,
+    );
+    const slider = new Slider();
+    slider.init();
+    // Вернуть this (jQuery объект)
+  };
+}(jQuery));
