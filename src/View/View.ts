9ee6b778333f5cd59b4bd8669865ddd20871532b
dywork <dubrovskywork@gmail.com>
2020-03-30 14:45:13 +0500

    feat(view): использую шаблон handlebar вместо DOM api

--- src/View/View.ts
+++ src/View/View.ts
@@ -1,6 +1,8 @@
 import Observer from '../Observer/Observer';
 import ISliderOptions from '../interfaces/ISliderOptions';
 
+const sliderTemplate = require('./template/sliderTemplate.hbs');
+
 interface IView {
   render(): void;
 }
@@ -8,38 +10,39 @@ interface IView {
 class View extends Observer implements IView {
   private viewOptions: ISliderOptions;
 
+  private viewMinValue: HTMLSpanElement;
+
+  private viewMaxValue: HTMLSpanElement;
+
+  private viewCurrentValue: HTMLSpanElement;
+
   constructor(viewOptions: ISliderOptions) {
     super();
     this.viewOptions = viewOptions;
   }
 
   redrawValue = (sliderOptions: ISliderOptions) => {
-    const viewMinValue = document.querySelector('#viewMinValue');
     const minValue = this.viewOptions.range.min;
-    viewMinValue.textContent = `Минимальное значение: ${minValue}`;
-    const viewMaxValue = document.querySelector('#viewMaxValue');
+    this.viewMinValue.textContent = `${minValue}`;
     const maxValue = this.viewOptions.range.max;
-    viewMaxValue.textContent = `Максимальное значение: ${maxValue}`;
-    const viewCurrentValue = document.querySelector('#viewCurrentValue');
-    viewCurrentValue.textContent = `Текущее значение: ${sliderOptions.currentValue}`;
+    this.viewMaxValue.textContent = `${maxValue}`;
+    this.viewCurrentValue.textContent = `${sliderOptions.currentValue}`;
   };
 
   render = () => {
-    const viewMinValue = this.getViewMinValue();
-    viewMinValue.id = 'viewMinValue';
-    const viewMaxValue = this.getViewMaxValue();
-    viewMaxValue.id = 'viewMaxValue';
-    const viewCurrentValue = this.getViewCurrentValue();
-    viewCurrentValue.id = 'viewCurrentValue';
-    const minusBtn = this.getBtn('-');
+    const sliderView = document.createElement('div');
+    sliderView.innerHTML = sliderTemplate({
+      currentValue: this.viewOptions.currentValue,
+      range: this.viewOptions.range,
+    });
+    document.body.appendChild(sliderView);
+    const minusBtn = document.querySelector('#rangeSliderBtnMinus');
+    const plusBtn = document.querySelector('#rangeSliderBtnPlus');
     minusBtn.addEventListener('click', this.onClickMinus);
-    const plusBtn = this.getBtn('+');
     plusBtn.addEventListener('click', this.onClickPlus);
-    document.body.appendChild(viewMinValue);
-    document.body.appendChild(viewMaxValue);
-    document.body.appendChild(viewCurrentValue);
-    document.body.appendChild(minusBtn);
-    document.body.appendChild(plusBtn);
+    this.viewMinValue = document.querySelector('#sliderMinValue');
+    this.viewMaxValue = document.querySelector('#sliderMaxValue');
+    this.viewCurrentValue = document.querySelector('#sliderCurrentValue');
   };
 
   private onClickMinus = (evt: Event) => {
@@ -56,32 +59,6 @@ class View extends Observer implements IView {
     const sliderOptions = this.viewOptions;
     sliderOptions.currentValue = newCurrentValue;
     this.notify('sliderOptionsUpdate', this.viewOptions);
-  };
-
-  private getViewCurrentValue = () => {
-    const viewCurrentValue = document.createElement('p');
-    viewCurrentValue.textContent = `Текущее значение: ${this.viewOptions.currentValue}`;
-    return viewCurrentValue;
-  };
-
-  private getViewMinValue = () => {
-    const viewMinValue = document.createElement('p');
-    const minValue = this.viewOptions.range.min;
-    viewMinValue.textContent = `Минимальное значение: ${minValue}`;
-    return viewMinValue;
-  };
-
-  private getViewMaxValue = () => {
-    const viewMaxValue = document.createElement('p');
-    const maxValue = this.viewOptions.range.max;
-    viewMaxValue.textContent = `Максимальное значение: ${maxValue}`;
-    return viewMaxValue;
-  };
-
-  private getBtn = (btnName: string) => {
-    const btn = document.createElement('button');
-    btn.textContent = btnName;
-    return btn;
   };
 }
 
