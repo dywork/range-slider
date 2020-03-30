8f6f4ee90e22d9ab39aa93bdaad664c12effba30
dywork <dubrovskywork@gmail.com>
2020-03-30 15:19:34 +0500

    refactor(presenter.test): использую spyOn вместо createSpy

--- src/Presenter/presenter.test.ts
+++ src/Presenter/presenter.test.ts
@@ -5,8 +5,8 @@ describe('Presenter', () => {
   it('инициализируется', () => {
     const sliderOptions: ISliderOptions = { currentValue: 0, range: { min: 0, max: 100 } };
     const presenter = new Presenter(sliderOptions);
-    const presenterInitSpy = jasmine.createSpy('init', presenter.init);
+    spyOn(presenter, 'init');
     presenter.init();
-    expect.call(presenterInitSpy);
+    expect.call(presenter.init);
   });
 });
