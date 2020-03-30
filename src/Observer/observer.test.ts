833c49fa8b1f7f05e0a39a6e7fdcb3b905e86235
dywork <dubrovskywork@gmail.com>
2020-03-30 16:21:40 +0500

    test(observer): добавил тест на оповещение

--- src/Observer/observer.test.ts
+++ src/Observer/observer.test.ts
@@ -8,4 +8,19 @@ describe('Observer', () => {
     observer.subscribe('testSub', testSubCallBack);
     expect(observer.subscribe).toHaveBeenCalledWith('testSub', testSubCallBack);
   });
+
+  it('оповещает', () => {
+    const observer = new Observer();
+    const testData = { text: 'hello from test' };
+    const testSubCallBack = jasmine.createSpy('testSubCallBack', (data: Object) => {
+      // eslint-disable-next-line no-console
+      console.log(data);
+    });
+    spyOn(observer, 'subscribe');
+    spyOn(observer, 'notify');
+    observer.subscribe('testSub', testSubCallBack);
+    observer.notify('testSub', testData);
+    expect(observer.subscribe).toHaveBeenCalledWith('testSub', testSubCallBack);
+    expect(observer.notify).toHaveBeenCalledWith('testSub', testData);
+  });
 });
