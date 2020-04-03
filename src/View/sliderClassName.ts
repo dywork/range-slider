30ac19b52a0fbe32bb3bd3c65d3894afa5ff33c1
dywork <dubrovskywork@gmail.com>
2020-04-03 13:34:33 +0500

    feat(sliderClassName): добавил объект с именами классов

---
+++ src/View/sliderClassName.ts
@@ -0,0 +1,21 @@
+interface ISliderClassName {
+  overflowFix: string;
+  slider: string;
+  wrap: string;
+  bar: string;
+  scale: string;
+  toggle: string;
+  handle: string;
+}
+
+const sliderClassName: ISliderClassName = {
+  overflowFix: 'range-slider-overflow',
+  slider: 'range-slider',
+  wrap: 'range-slider__wrap',
+  bar: 'range-slider__bar',
+  scale: 'range-slider__scale',
+  toggle: 'range-slider__toggle',
+  handle: 'range-slider__handle',
+};
+
+export default sliderClassName;
