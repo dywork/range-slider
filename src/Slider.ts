d90a45bf3ffb014c4a8c11c0aba48058615c15a7
dywork <dubrovskywork@gmail.com>
2020-04-04 10:42:20 +0500

    feat(slider): раздиляю sliderOptions на viewOptions и modelOptions

--- src/Slider.ts
+++ src/Slider.ts
@@ -1,17 +1,38 @@
 import Presenter from './Presenter/Presenter';
-import ISliderOptions from './interfaces/ISliderOptions';
+import { IViewOptions } from './View/View';
 import './style/style.scss';
+
+interface ISliderOptions {
+  domParent: HTMLElement;
+  currentValue: number;
+  range: { min: number; max: number };
+  isThumb: boolean;
+}
 
 class Slider {
   private presenter: Presenter;
 
-  constructor(domParent: HTMLElement, sliderOptions: ISliderOptions) {
-    this.presenter = new Presenter(domParent, sliderOptions);
+  constructor(sliderOptions: ISliderOptions) {
+    const viewOptions: IViewOptions = this.getViewOptions(sliderOptions);
+    const modelOptions = this.getModelOptions(sliderOptions);
+    this.presenter = new Presenter(viewOptions, modelOptions);
   }
 
   init = () => {
     this.presenter.init();
   };
+
+  private getViewOptions = (sliderOptions: ISliderOptions) => {
+    const { domParent, isThumb } = sliderOptions;
+    const viewOptions: IViewOptions = { domParent, isThumb };
+    return viewOptions;
+  };
+
+  private getModelOptions = (sliderOptions: ISliderOptions) => {
+    const { currentValue, range } = sliderOptions;
+    const modelOptions = { currentValue, range };
+    return modelOptions;
+  };
 }
 
-export default Slider;
+export { Slider, ISliderOptions };
