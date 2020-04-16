2014cfa9404fc6e6920a36d1647c27f17074837a
dywork <dubrovskywork@gmail.com>
2020-04-16 16:49:14 +0500

    feat(defaultOptions): вынес дефолтные настройки в отдельный объект

---
+++ src/defaultOptions.ts
@@ -0,0 +1,13 @@
+const defaultOptions = {
+  currentValue: 0,
+  range: {
+    min: 0,
+    max: 100,
+  },
+  isThumb: true,
+  step: 0,
+  decimal: 2,
+  orientation: 'horizontal',
+};
+
+export default defaultOptions;
