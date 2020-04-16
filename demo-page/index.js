8c00f59706eb33c72d46586eaa5c54e3a3e833ae
dywork <dubrovskywork@gmail.com>
2020-04-16 16:47:52 +0500

    feat(demo-index): добавил настройки для слайдеров

--- demo-page/index.js
+++ demo-page/index.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
-  $('#firstSlider').rangeSlider({ currentValue: 91, range: { min: 0, max: 100 }, isThumb: true });
-  $('#secondSlider').rangeSlider({ currentValue: 20, range: { min: 0, max: 100 }, isThumb: true });
+$(document).ready(function () {
+  $('#firstSlider').rangeSlider({ currentValue: 15, range: { min: 13, max: 30 }, isThumb: true });
+
+  $('#secondSlider').rangeSlider({
+    currentValue: 3,
+    range: { min: 3, max: 19 },
+    isThumb: true,
+    step: 5,
+  });
+
+  $('#thirdSlider').rangeSlider({
+    currentValue: 15,
+    range: { min: 13, max: 30 },
+    isThumb: true,
+    orientation: 'vertical',
+  });
+
+  $('#fourthSlider').rangeSlider({
+    currentValue: 3,
+    range: { min: 3, max: 19 },
+    isThumb: true,
+    step: 5,
+    orientation: 'vertical',
+  });
 });
