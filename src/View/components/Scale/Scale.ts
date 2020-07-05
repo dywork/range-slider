8a57aba3e0df91530ef6f33eea35edf36131f816
dywork <dubrovskywork@gmail.com>
2020-07-05 16:40:56 +0500

    добавил Scale методы расчета его размера

--- src/View/components/Scale/Scale.ts
+++ src/View/components/Scale/Scale.ts
@@ -1,13 +1,41 @@
+import { IModelOptions } from '../../../Model/Model';
 import sliderClassName from '../../sliderClassName';
 
 const scaleTemplate = require('./template.hbs');
 
 class Scale {
+  private modelOptions: IModelOptions;
+
+  private isVertical: boolean;
+
+  constructor(modelOptions: IModelOptions, isVertical: boolean) {
+    this.modelOptions = modelOptions;
+    this.isVertical = isVertical;
+  }
+
   getHtml = () => {
     const templateOptions = { sliderClassName };
     const scale = document.createElement('div');
     scale.innerHTML = scaleTemplate(templateOptions);
+    const scaleView = scale.querySelector(`.${sliderClassName.scale}`);
+    scaleView.setAttribute('style', this.getScaleTransformStyle());
     return scale.firstChild;
+  };
+
+  private getScaleTransformStyle = () => {
+    const scalePosition = this.getScalePosition();
+
+    if (this.isVertical) {
+      return `transform: scale(1, ${scalePosition});`;
+    }
+
+    return `transform: scale(${scalePosition}, 1);`;
+  };
+
+  private getScalePosition = () => {
+    const { currentValue, range } = this.modelOptions;
+    const scalePosition = (+currentValue - range.min) / (range.max - range.min);
+    return scalePosition;
   };
 }
 
