8f42d87072233803126654b8394c36580536822a
dywork <dubrovskywork@gmail.com>
2020-07-09 11:44:17 +0500

    добавил метод расчета значений Ruler

--- src/View/components/Ruler/Ruler.ts
+++ src/View/components/Ruler/Ruler.ts
@@ -15,10 +15,24 @@ class Ruler {
   }
 
   getHtml = () => {
-    const templateOptions = { sliderClassName };
+    const templateOptions = { sliderClassName, values: this.getRulerValues() };
     const ruler = document.createElement('div');
     ruler.innerHTML = rulerTemplate(templateOptions);
     return ruler.firstChild;
+  };
+
+  private getRulerValues = () => {
+    const { range, step } = this.props;
+    const middArr = [];
+    const midQuantity = Math.floor((range.max - range.min) / step);
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < midQuantity; i++) {
+      const value = (i + 1) * step + range.min;
+      middArr.push(value);
+    }
+
+    return [range.min, ...middArr, range.max];
   };
 }
 
