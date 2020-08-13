f6d682fdcf166ed40822af05d96ca78fe2ef5406
dywork <dubrovskywork@gmail.com>
2020-08-13 16:02:10 +0500

    соендинил demo/index со Slider

--- src/demo-page/index.ts
+++ src/demo-page/index.ts
@@ -1,30 +1,31 @@
-import './configPanel';
+import { Slider, ISliderOptions } from '../slider/Slider';
+import defaultOptions from '../slider/defaultOptions';
+import './style.scss';
 
-// import { Slider, ISliderOptions } from '../slider/Slider';
-// import defaultOptions from '../slider/defaultOptions';
+declare global {
+  interface JQuery {
+    rangeSlider(options: ISliderOptions): void;
+  }
+}
 
-// declare global {
-//   interface JQuery {
-//     rangeSlider(options: ISliderOptions): void;
-//   }
-// }
+(function (jquery) {
+  const $ = jquery;
 
-// (function (jquery) {
-//   const $ = jquery;
+  $.fn.rangeSlider = function (options: ISliderOptions) {
+    let sliderOptions = options;
+    sliderOptions = $.extend(
+      {
+        domParent: this[0],
+        ...defaultOptions,
+      },
+      sliderOptions
+    );
 
-//   $.fn.rangeSlider = function (options: ISliderOptions) {
-//     let sliderOptions = options;
-//     sliderOptions = $.extend(
-//       {
-//         domParent: this[0],
-//         ...defaultOptions,
-//       },
-//       sliderOptions,
-//     );
+    const slider = new Slider(sliderOptions);
+    slider.init();
 
-//     const slider = new Slider(sliderOptions);
-//     slider.init();
+    return slider;
+  };
+})(jQuery);
 
-//     return slider;
-//   };
-// }(jQuery));
+import './demoPage';
