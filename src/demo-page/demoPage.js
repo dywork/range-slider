b160e36c6c64ec0f52d4060a077c725b9a28763b
dywork <dubrovskywork@gmail.com>
2020-08-14 19:10:30 +0500

    инициализирую configPanel

--- src/demo-page/demoPage.js
+++ src/demo-page/demoPage.js
@@ -1,60 +1,10 @@
 $(document).ready(function () {
-  $('#firstSlider').rangeSlider({
+  const defaultSlider = $('#defaultSlider').rangeSlider({
     currentValue: 15,
     range: { min: 10, max: 30 },
     isRuler: true,
     isThumb: true,
   });
 
-  $('#secondSlider').rangeSlider({
-    currentValue: 3,
-    range: { min: 3, max: 19 },
-    isRuler: true,
-    isThumb: true,
-    step: 5,
-  });
-
-  $('#thirdSlider').rangeSlider({
-    currentValue: 15,
-    range: { min: 13, max: 30 },
-    isThumb: true,
-    orientation: 'vertical',
-  });
-
-  $('#fourthSlider').rangeSlider({
-    currentValue: 3,
-    range: { min: 3, max: 19 },
-    isThumb: true,
-    isRuler: true,
-    step: 5,
-    orientation: 'vertical',
-  });
-
-  $('#fifthSlider').rangeSlider({
-    currentValue: [15, 27],
-    range: { min: 13, max: 30 },
-    isThumb: true,
-  });
-
-  $('#sixthSlider').rangeSlider({
-    currentValue: [15, 27],
-    range: { min: 13, max: 30 },
-    step: 5,
-    isThumb: true,
-  });
-
-  $('#seventhSlider').rangeSlider({
-    currentValue: [15, 27],
-    range: { min: 13, max: 30 },
-    isThumb: true,
-    orientation: 'vertical',
-  });
-
-  $('#eighthSlider').rangeSlider({
-    currentValue: [15, 27],
-    range: { min: 13, max: 30 },
-    step: 5,
-    isThumb: true,
-    orientation: 'vertical',
-  });
+  $('#defaultSliderConfig').configPanel(defaultSlider);
 });
