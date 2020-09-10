2eccddd164016c629d0a708b5e8c63176c9e576f
dywork <dubrovskywork@gmail.com>
2020-09-10 13:04:53 +0500

    перенес код index.ts из demo-page в Index приложения

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-import './demo-page/index';
+/* eslint-disable func-names */
+import { Slider, ISliderOptions } from './slider/Slider';
+import ConfigPanel from './demo-page/СonfigPanel';
+import defaultOptions from './slider/defaultOptions';
+import './demo-page/style.scss';
+import './demo-page/demoPage';
+
+declare global {
+  interface JQuery {
+    rangeSlider(options: ISliderOptions): Slider;
+    configPanel(rangeSlider: Slider): ConfigPanel;
+  }
+}
+
+(function (jquery) {
+  const $ = jquery;
+
+  $.fn.rangeSlider = function (options: ISliderOptions) {
+    let sliderOptions = options;
+    sliderOptions = $.extend(
+      {
+        domParent: this[0],
+        ...defaultOptions,
+      },
+      sliderOptions,
+    );
+
+    const slider = new Slider(sliderOptions);
+    slider.init();
+
+    return slider;
+  };
+
+  $.fn.configPanel = function (rangeSlider: Slider) {
+    const configPanel = new ConfigPanel(this[0], rangeSlider);
+    configPanel.init();
+
+    // rangeSlider.subscribe('sliderOptionsUpdate', (value: number) => {
+    //   console.log(value);
+    // });
+
+    return configPanel;
+  };
+}(jQuery));
