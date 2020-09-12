cf45e85eff7e78b5ad67ba9fe7574b394668e064
dywork <dubrovskywork@gmail.com>
2020-09-12 11:55:51 +0500

    добавил слайдер с диапазоном на страницу

--- src/demo-page/demoPage.js
+++ src/demo-page/demoPage.js
@@ -6,5 +6,13 @@ $(document).ready(function () {
     isThumb: true,
   });
 
+  const rangeSlider = $('#rangeSlider').rangeSlider({
+    currentValue: [15, 25],
+    range: { min: 10, max: 30 },
+    isRuler: true,
+    isThumb: true,
+  });
+
   $('#defaultSliderConfig').configPanel(defaultSlider);
+  $('#rangeSliderConfig').configPanel(rangeSlider);
 });
