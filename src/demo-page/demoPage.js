4c827e870ac4d1c19b94d9990243befcf0f9f2e9
dywork <dubrovskywork@gmail.com>
2020-09-15 13:38:29 +0500

    добавил на страницу вертикальный слайдер

--- src/demo-page/demoPage.js
+++ src/demo-page/demoPage.js
@@ -13,6 +13,15 @@ $(document).ready(function () {
     isThumb: true,
   });
 
+  const verticalSlider = $('#verticalSlider').rangeSlider({
+    currentValue: 15,
+    range: { min: 10, max: 30 },
+    isRuler: true,
+    isThumb: true,
+    orientation: 'vertical',
+  });
+
   $('#defaultSliderConfig').configPanel(defaultSlider);
   $('#rangeSliderConfig').configPanel(rangeSlider);
+  $('#verticalSliderConfig').configPanel(verticalSlider);
 });
